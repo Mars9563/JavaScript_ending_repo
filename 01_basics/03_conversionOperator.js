250da2bec68cb04ed660176281a4a38cfc18ee57
Mradul raj Singh <168324728+Mars9563@users.noreply.github.com>
2025-06-05 21:09:37 +0000

    conversion continuation and increment with prefix and postfix and same for decrement

--- 01_basics/03_conversionOperator.js
+++ 01_basics/03_conversionOperator.js
@@ -20,7 +20,7 @@ let score = "33abc";
 let val = undefined;
 
 let boolVal = Boolean(val);
-console.log(boolVal);
+// console.log(boolVal);
 
 // non-zero = true; 0 = false;
 // "" = false; "anything" = 1;
@@ -32,5 +32,40 @@ let someNumber = 33;
 
 let stringNumber = String(someNumber);
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ********* Operations **********
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2);
+// console.log(2 / 3);
+// console.log(2 % 3);
+
+let str1 = "Hello";
+let str2 = " Mradul";
+
+let str3 = (str1+str2)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100;
+
+console.log(gameCounter++);
+// i know ++x and x++ what they are;
+// but if someone else is reading this then
+// Let me expalin, let x = 1
+// so if you write ++x it means => increase x and then use it
+// and if you write x++ it means => use x now and after that increase it
+// so c = ++x + 2 => 4
+// but if c = x++ + 2 => 3
