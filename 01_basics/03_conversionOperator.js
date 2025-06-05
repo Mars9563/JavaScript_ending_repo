44c85cdea05976d0daaef24b28d72aef1f4d8089
Mradul raj Singh <168324728+Mars9563@users.noreply.github.com>
2025-06-05 20:47:47 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperator.js
@@ -0,0 +1,36 @@
+let score = "33abc";
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// in this case valueInNumber contains NaN
+// it is a number type but we will talk later
+
+// "33" => 33
+// "33abc" = NaN
+// true = 1; false = 0;
+// null = 0;
+// undefined  = NaN
+
+let val = undefined;
+
+let boolVal = Boolean(val);
+console.log(boolVal);
+
+// non-zero = true; 0 = false;
+// "" = false; "anything" = 1;
+// null = false;
+// undefined = false
+
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
