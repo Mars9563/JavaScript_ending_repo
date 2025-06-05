6be12abf806b36a67e6c20935e589d9595310dc1
Mradul raj Singh <168324728+Mars9563@users.noreply.github.com>
2025-06-05 21:48:49 +0000

    interview prep video small small stuff

---
+++ datatypes-summary.js
@@ -0,0 +1,28 @@
+//datatype
+// divided only in two
+// primitive
+// |
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+// symbol
+const id = Symbol('123')
+const anotherId = Symbol('123');
+
+console.log(id === anotherId);
+
+const bigNumber = 2348039480239840982023984n;
+
+
+// Refrence(Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+const obj = {
+    Name : "Mradul",
+    age : 22,
+};
+
+const myFunction  = function(){
+    console.log("hello world!");
+};
+// Dynamically types language 
