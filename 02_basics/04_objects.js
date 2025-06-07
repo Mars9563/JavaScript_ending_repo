b19eb7e158897794a8062c06fcd6f2bc8f53df12
Mradul raj Singh <168324728+Mars9563@users.noreply.github.com>
2025-06-07 07:44:27 +0000

    Learn objects part-2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,19 @@
+// const tinderUser = new Object();
+const tinderUser = {};
+
+tinderUser.id = "123abc";
+tinderUser.name = "samy";
+tinderUser.isLoggedIn = false;
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        firstname: "Mradul",
+        middlename: "Raj",
+        lastname: "Singh"
+    },
+}
+
+console.log();
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # JavaScript_ending_repo
 i have to complete this in a day
+
+I thought i will be able to complete in one day
+hehe
+lets see how many days it takes
