25925abc5072c86d8256730d837f50683d319f76
Mradul Raj Singh <mradulrajsingh61@gmail.com>
2026-04-23 21:00:52 +0530

    at lesson no. 5

---
+++ chapters/ch_1.js
@@ -0,0 +1,14 @@
+console.log("Founder of node js and deno js is Ryan Dahl");
+
+// variables
+const accountId = 12345;
+let accountEmail = "mradulrajsingh61@gmail.com";
+var accountPassword = "1234567890";
+accountCity = "Moradabad";
+let accountState;
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+// prefer not to use var
+// because of the issue in the block scope and functional scope
+
