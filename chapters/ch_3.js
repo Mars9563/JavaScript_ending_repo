1a2f75e557cdbb21849e59cbd567bc1837464199
Mradul Raj Singh <mradulrajsingh61@gmail.com>
2026-04-23 21:22:21 +0530

    at lesson no.6

---
+++ chapters/ch_3.js
@@ -0,0 +1,25 @@
+let score = "33abc";
+
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// null = 0
+// string = NaN
+// true = 1
+// false = 0
+// -------
+
+// Boolean
+// 1 = true; 0 = false;
+// "" = false
+// "hitesh" = true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
